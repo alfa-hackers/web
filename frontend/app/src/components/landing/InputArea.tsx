842a417820d1a6673b820a5d0b8f0b16b8585275
vwency <veniaminshp@gmail.com>
2025-11-14 12:53:55 +0600

    refactor(styles): decomposition

--- frontend/app/src/components/landing/InputArea.tsx
+++ frontend/app/src/components/landing/InputArea.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect } from 'react'
-import '../../styles/landing/landing.scss'
+import '../../styles/landing/inputarea.scss'
 import {
   FileAttachment,
   MessageFlag,
